ba6928ef5a89683f6eacff91975b11dca0f26935
= <=>
2026-09-18 19:05:50 -0500

    feat: agrega  GET /students

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI(title="students-api")
 @app.get("/health")
 def health():
     return {status: "ok"}
+
+@app.get("/students")
+def list_students()
+    return[{"id":1, "name":"Ana"}, {"id":2, "name":"Armando"}]
